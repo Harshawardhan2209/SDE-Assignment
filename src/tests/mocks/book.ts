0f3aaaa7a1ba1054f79886d6b1a4ff7919636c1a
Harshavardhan Patare <harshvardhanpatare@gmail.com>
2025-08-26 17:04:05 +0530

    test: add test mocks and helpers under src/tests/

---
+++ src/tests/mocks/book.ts
@@ -0,0 +1,13 @@
+import { IBook } from '@/types';
+
+export const mockBook: IBook = {
+  id: 1,
+  title: 'Test Book',
+  author: 'Test Author',
+  price: 29.99,
+  description: 'Test description',
+  rating: 4.5,
+  reviewCount: 10,
+  pages: 300,
+  genre: 'Fiction',
+};
